1f43c7aa59b62058e80a685d7b03e4d035b691dc
rioil <chrio.contact+github@gmail.com>
2018-03-12 11:27:16 +0900

    command部分の分離

---
+++ src/command/tmake.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace team\command;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\PluginIdentifiableCommand;
+use pocketmine\Player;
+use pocketmine\plugin\Plugin;
+
+class tmaeke extends Command{
+
+}
