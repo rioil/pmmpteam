fe6160e9a15aa0be5f2928a5ca7208655f66de54
rioil <chrio.contact+github@gmail.com>
2018-03-12 11:33:59 +0900

    コマンド関連の修正

--- src/command/tmake.php
+++ src/command/tmake.php
@@ -10,4 +10,42 @@ use pocketmine\plugin\Plugin;
 
 class tmaeke extends Command{
 
+    //コマンドの処理
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+          
+        //引数が空のとき
+        if(count($args) === 0){
+
+          $sender->sendMessage("Usage:  /tmeke [TeamName]");
+          return false;
+
+        }
+
+        //OPが実行したとき
+        if($sender->isOp()){
+
+          $username = strtolower($args[0]);
+          $player = $sender->getServer()->getPlayer($username);
+
+          if($player instanceOf Player){
+
+            //$playerip = $player->getAddress();
+            //$sender->sendMessage("".$player->getPlayer()->getName()." is IP:".$playerip."");
+            //return true;
+
+          }else{
+
+            //$sender->sendMessage("".$username." doesn't exist");
+            //return true;
+
+          }
+
+        //OP以外が実行したとき
+        }else{
+
+          $sender->sendMessage("You don't have permission.");
+          return true;
+
+        }
+    }
 }
--- src/team/team.php
+++ src/team/team.php
@@ -2,14 +2,20 @@
 
 namespace team;
 
+use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Server;
 
 class team extends PluginBase{
 
+
+        //plugin読み込み時に実行
         public function onLoad(){
                 $this->getLogger()->info("onLoad() has been called!");
         }
-
+        //pluginが有効になった時に実行
         public function onEnable(){
                 $this->getLogger()->info("onEnable() has been called!");
         }
